0576ea725db2b8c604b9caedad9555af62821249
Andrey Arzamaskov <arzamaskov@gmail.com>
2023-02-21 11:23:25 +0400

    create project files

---
+++ app/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$templates_path = __DIR__ . '/assets/templates/';
+
+$web_server = getenv('SERVER_SOFTWARE');
+$php_version = phpversion();
+
+$link = mysqli_connect('database', 'admin', 'root', null);
+
+if (mysqli_connect_errno()) {
+    $mysql = sprintf(
+        'MySQL connecttion failed: %s',
+        mysqli_connect_error()
+    );
+} else {
+    $mysql = sprintf('MySQL Server: %s', mysqli_get_server_info($link));
+}
+
+mysqli_close($link);
+
+include $templates_path . 'index.html';
---
+++ app/test_db_pdo.php
@@ -0,0 +1,15 @@
+<?php
+
+$user = 'root';
+$password = 'root';
+$pdo = null;
+
+try {
+    $database = 'mysql:host=database:3306';
+    $pdo = new PDO($database, $user, $password);
+    echo 'Success: A proper connection to MySQL was made! The docker database is great.';
+} catch (PDOException $e) {
+    echo "Error: Unable to connect to MySQL. Error:\n $e";
+}
+
+$pdo = null;
